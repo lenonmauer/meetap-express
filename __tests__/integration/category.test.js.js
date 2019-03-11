8e1038131f0a30ecc22d7de814be291b8e7b9268
Lenon <lenonmauer@gmail.com>
2019-03-11 14:20:11 -0300

    added category controller test

---
+++ __tests__/integration/category.test.js.js
@@ -0,0 +1,22 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+
+const app = require('../../src/server');
+
+const { expect } = chai;
+
+chai.use(chaiHttp);
+
+describe('Category Controller', () => {
+  describe('Index', () => {
+    it('should return all categories', async () => {
+      const response = await chai.request(app)
+        .get('/categories')
+        .send();
+
+      expect(response.status).to.be.eq(200);
+      expect(response.body.length).to.be.eq(6);
+      expect(response.body[0]).to.have.property('id');
+      expect(response.body.length).to.have.property('name');
+    });
+});
