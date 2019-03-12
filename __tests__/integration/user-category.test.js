62c50768f89988a823f8127b1ee30b56ae309c17
Lenon <lenonmauer@gmail.com>
2019-03-11 22:56:01 -0300

    added user category test

---
+++ __tests__/integration/user-category.test.js
@@ -0,0 +1,50 @@
+const mongoose = require('mongoose');
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+
+const app = require('../../src/server');
+
+const User = mongoose.model('User');
+
+const factory = require('../factories');
+const { expect } = chai;
+
+chai.use(chaiHttp);
+
+describe('UserCategory Controller', () => {
+  beforeEach(async () => {
+    await User.deleteMany();
+  });
+
+  describe('Update', () => {
+    it('should be able to update user categories', async () => {
+      const user = await factory.create('User');
+      const token = user.generateToken();
+      const categories = [
+        'front-end',
+        'back-end',
+      ];
+
+      const response = await chai.request(app)
+        .put('/user-categories')
+        .set('Authorization', `Bearer ${token}`)
+        .send({ categories });
+
+      expect(response.status).to.be.eq(200);
+      expect(response.body).to.have.property('name');
+      expect(response.body).to.have.property('categories');
+    });
+
+    it('should return validation errors when no data was sent', async () => {
+      const user = await factory.create('User');
+      const token = user.generateToken();
+
+      const response = await chai.request(app)
+        .put('/user-categories')
+        .set('Authorization', `Bearer ${token}`)
+        .send();
+
+      expect(response.status).to.be.eq(422);
+    });
+  });
+});
