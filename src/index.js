c4836077cbfde855b2d3ecce0018383deaaa829b
Lenon <lenonmauer@gmail.com>
2019-03-14 00:20:37 -0300

    changed default port to 3333

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const server = require('./server');
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3333;
 
 server.listen(port, () => {
   console.log(`Listening at ${port}`);
